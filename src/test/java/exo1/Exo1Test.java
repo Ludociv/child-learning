9f0ae402617d0dee9dc9691f9bed02035fa0e58f
David Avenante <altus34@gmail.com>
2019-01-13 21:43:08 -0500

    First import

---
+++ src/test/java/exo1/Exo1Test.java
@@ -0,0 +1,13 @@
+package exo1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Exo1Test {
+    @Test
+    @DisplayName("THis test should display the sentence 'Hello World' on console")
+    void output() {
+        // Take a look aon the class System to write the sentence in the console
+        // System;
+    }
+}
